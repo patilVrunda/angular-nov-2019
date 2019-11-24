55efad60e76d875c7cf560191f1e12de7f2d7ddd
unknown <you@example.com>
2019-11-24 11:48:31 +0530

    add paramas file

---
+++ ts/params.ts
@@ -0,0 +1,25 @@
+export class SqlTable {
+
+    private _sql: string
+
+        get sql() {
+            return this._sql
+        }
+
+        set sql(sql: string) {
+            this._sql = sql
+        }
+
+        query(hi?: string, sql:string = "abc"){//optional and default params
+
+            console.log(`Executing SQL ${sql}`);
+            
+
+        }
+}
+
+let sqlTab: SqlTable = new SqlTable()
+sqlTab.sql = 'select * from tabs'
+console.log(sqlTab.sql)
+sqlTab.query(`slvvjslk`)
+sqlTab.query()
